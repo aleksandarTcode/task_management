7a1b6e5c0048ee0cb4de87f892d7f819f01008f8
aleksandarT <aleksandar.trmcic@gmail.com>
2023-07-26 09:42:48 +0200

    tasks and users migration created

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get('/products',function (){
+    return 'product';
+});
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
